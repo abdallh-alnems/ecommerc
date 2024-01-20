1fe6358eec9bec5a38b7be0913e69f00a5d6f038
abdallh-alnems <abdallhmoustafa295@gmail.com>
2024-01-20 21:43:50 +0200

    language

--- lib/core/constant/routes/routes.dart
+++ lib/core/constant/routes/routes.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import '../../../view/screen/auth/login.dart';
+import '../../../view/screen/onboarding.dart';
 import 'app_routes.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
-  AppRoute.login : (context) => const Login()
+  AppRoute.login : (context) => const Login(),
+  AppRoute.onBoarding : (context) => const OnBoarding(),
 };
--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-
 import 'core/constant/color.dart';
 import 'core/constant/routes/routes.dart';
+import 'core/localization/changelocal.dart';
+import 'core/localization/translation.dart';
+import 'core/services/services.dart';
+import 'view/screen/language.dart';
 import 'view/screen/onboarding.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initialServices();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return  GetMaterialApp(
-        debugShowCheckedModeBanner: false,
+    LocaleController controller = Get.put(LocaleController());
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'ecommerc',
-       theme: ThemeData(
+      theme: ThemeData(
         fontFamily: "PlayfairDisplay",
-        textTheme:  const TextTheme(
-            displayLarge: TextStyle(fontWeight: FontWeight.bold, fontSize: 20 , color: AppColor.black),
+        textTheme: const TextTheme(
+            displayLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+                color: AppColor.black),
             bodyLarge: TextStyle(
                 height: 2,
                 color: AppColor.grey,
@@ -29,9 +37,10 @@ class MyApp extends StatelessWidget {
                 fontSize: 17)),
         primarySwatch: Colors.blue,
       ),
-      home: OnBoarding(),
+      translations: MyTranslation(),
+      locale: controller.language,
+      home: Language(),
       routes: routes,
     );
   }
 }
-
